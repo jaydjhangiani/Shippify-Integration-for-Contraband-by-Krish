13462066a33c5219c933445f95726c5a632a38ac
Jay Jhangiani <jaydjhangiani@gmail.com>
2022-03-06 00:30:30 +0530

    implemented loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+const Loader = () => {
+  return (
+    <center
+      style={{
+        display: "grid",
+        placeItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>Loading....</div>
+    </center>
+  );
+};
+
+export default Loader;
